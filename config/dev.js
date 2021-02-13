dae0f5511f1162c09e1c5e461399ec1cfe33f042
Byunwoojin <sforot@naver.com>
2021-02-13 23:26:52 +0900

    비밀 정보 보호

---
+++ config/dev.js
@@ -0,0 +1,3 @@
+module.exports={
+    mongoURI:'mongodb+srv://snowwoo:qaz7418308!@boilerplate.ztt9k.mongodb.net/<dbname>?retryWrites=true&w=majority'
+}
--- index.js
+++ index.js
@@ -1,18 +1,40 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require("body-parser");
+const{User}=require("./models/User");
+const config = require('./config/key');
+
+// application/x-222-from-urlencoded 분석해서 가져옴
+app.use(bodyParser.urlencoded({extended:true}));
+
+// application/json 분석해서 가져옴
+app.use(bodyParser.json());
+
 
 const mongoose=require('mongoose')
-mongoose.connect('mongodb+srv://snowwoo:qaz7418308!@boilerplate.ztt9k.mongodb.net/<dbname>?retryWrites=true&w=majority'
+mongoose.connect(config.mongoURI
     , { userNewUrlParser:true, useUnifiedTopology:true, useCreateIndex: true, useFindAndModify: false
 
     }).then(()=>console.log("MongoDB Connected"))
     .catch(err=> console.log(err))
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello World! 새해복 많이 받으세요~')
 })
 
+app.post('/register',(req,res)=>{
+    // 회원 가입 할때 필요한 정보들을 client 에서 가져오면
+    // 그것들을 데이터 베이스에 넣어준다.
+    const user = new User(req.body)
+    // 정보가 user모델에 저장됨
+    user.save((err,userInfo)=>{
+        if(err)return res.json({success:false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
